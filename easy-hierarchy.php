095979bdd2965ba131d47ccd8261cabad0f43f2d
Marco Milesi <milesimarco@users.noreply.github.com>
2025-05-29 14:13:56 +0200

    Update easy-hierarchy.php

--- easy-hierarchy.php
+++ easy-hierarchy.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Easy Hierarchy
 Description: Makes WordPress page hierarchy management easy and intuitive with enhanced filtering and visual hierarchy indicators
-Version: 2.0.2
+Version: 2.0.3
 Author: Marco Milesi
 Author URI: https://www.marcomilesi.com
 License: GPL Attribution-ShareAlike
